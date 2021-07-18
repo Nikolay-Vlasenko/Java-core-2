8e12428e8bbead0bd09f63fcbe10bcb795e9a594
Nikolay-Vlasenko <fostrix@mail.ru>
2021-07-18 20:27:26 +0300

    Home work Lesson-2

---
+++ src/main/java/ru/geekbrains/java_core2/lesson2/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.java_core2.lesson2;
+
+public class MyArrayDataException extends Exception {
+
+    public MyArrayDataException() {
+    }
+}
---
+++ src/main/java/ru/geekbrains/java_core2/lesson2/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.java_core2.lesson2;
+
+public class MyArraySizeException extends Exception {
+
+    public MyArraySizeException() {
+    }
+}
